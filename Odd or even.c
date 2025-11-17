cb8f66990bee52c59be9fcdc4d59a0e84e5af32c
vinayai89 <vinayvinaysk77@gmail.com>
2025-11-17 22:08:24 +0530

    Odd or even.c

---
+++ Odd or even.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main()
+{
+     int a;
+     printf("enter a number\n");
+     scanf("%d",&a);
+     if(a%2==0)
+     printf("odd number");
+     else
+     printf("even number");
+}
