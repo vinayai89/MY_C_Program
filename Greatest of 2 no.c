edf69e7ceee300ddf1e51a6a3bc660ae865e90e7
vinayai89 <vinayvinaysk77@gmail.com>
2025-11-18 21:49:06 +0530

    Add program to find the greatest of two numbers

---
+++ Greatest of 2 no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   int a,b;
+    printf("enter value of a,b\n");
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("a is larger");
+
+    }
+    else{
+        printf("b is larger");
+    }
+}
