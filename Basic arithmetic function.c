74c7e6dfc5312cf95c9816e37c8b5c704cbdb5df
vinayai89 <vinayvinaysk77@gmail.com>
2025-11-18 21:40:48 +0530

    Create Basic arithmetic function.c

---
+++ Basic arithmetic function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int a,b,sum,prod,diffrence,quotient;
+    printf("enter value of a,b\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    prod=a*b;
+    diffrence=a-b;
+    quotient=a/b;
+    printf("sum=%d",sum);
+    printf("prod=%d",prod);
+    printf("diffrence=%d",diffrence);
+    printf("quotient=%d",quotient);
+}
