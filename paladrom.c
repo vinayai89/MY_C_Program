c950fe6b3f259ad492f6a2dad3c2e1ee89eb0d1e
vinayai89 <vinayvinaysk77@gmail.com>
2025-11-20 11:06:12 +0530

    Create paladrom.c

---
+++ paladrom.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num=777,org,rev=0,rem;
+    org=num;
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    if(org==rev){
+        printf("num is pal");
+    }
+    else{
+        printf("not pal");
+}
